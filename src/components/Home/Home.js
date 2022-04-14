cedd2218840a45d0dc2b5b84d24bde5a7666d789
jaykakkar31 <jaykakkar31@gmail.com>
2022-04-15 02:15:21 +0530

    changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
 import ScrollButton from "../scrollToTop";
-import Chatbotbtn from "../../chatbot/Chatbotbtn";
 import "./home.css";
 
 const Home = () => {
@@ -1521,9 +1520,7 @@ const Home = () => {
         </div>
       </section>{" "}
       <ScrollButton scrollState={scrollState} />
-      <div>
-        <Chatbotbtn />
-      </div>
+      
       <Footer />
     </div>
   );
